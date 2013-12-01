8cd7faaa77f9ba54f0f16a50bd7192d3e4e01576
Phuc <phuc@pxnguyen.com>
2013-12-01 03:59:21 -0800

    a

--- charDetMixtures.m
+++ charDetMixtures.m
@@ -28,73 +28,65 @@ configs = configsgen;
 [hms,scales] = getHeatmapMixtures(I,models,configs);
 end
 
-% function bbs=getbbs(I,models)
-% % Return the bbs from the previously collected hms
-% configs = configsgen;
-% nFrames = vidobject.NumberOfFrames;
-% new_temp_folder = fullfile('temp','bbs',vidobject.Name);
-% if ~exist(new_temp_folder,'dir')
-%     mkdir(new_temp_folder)
-% end
-% savebbs = @(sf,bbs) save(sf,'bbs');
-% parfor frame_index=1:nFrames
-%     fprintf('Working on frame %d...',frame_index);
-%     
-%     hms_path = fullfile('temp','hms',vidobject.Name);
-%     % load the hms
-%     lstruct = load(fullfile(hms_path,sprintf('%d.mat',frame_index)));
-%     hms = lstruct.hms;
-%     scales = lstruct.scales;
-%     sf = fullfile(new_temp_folder,sprintf('%d.mat',frame_index));
-%     if exist(sf,'file') > 0; fprintf('Skipped\n'); continue; end;
-%     tic; bbs = getbbsHelper(models,hms,configs); toc;
-%     
-%     % Save the bbs
-%     savebbs(sf,bbs);
-% end
-% bbs = [];
-% end
-% 
-% function bbs=getbbsHelper(models,hms,configs)
-% initthres = configs.initThres;
-% total_bbs = zeros(1e6,6);
-% total = 0;
-% 
-% %TODO: need to fix this
-% nScales = size(scales);
-% for level=1:nScales
-%     hms_scale = hms{level};
-%     if isempty(hms_scale); continue; end;
-%     current_scale = scales(level);
-%     for model_index = 1:length(models)
-%         char_dims = models{model_index}.char_dims;
-%         hm = hms_scale{model_index} + models{model_index}.bias;
-%         
-%         ind = find(hm > initthres); % Get the locations of the response
-%         [y,x] = ind2sub(size(hm),ind);
-%         if (isempty(x)); continue; end;
-%         
-%         scores = hm(ind);
-%         if (size(x,2) > 1); x = x'; y = y'; scores = scores'; end
-%         %assert(length(x)==length(scores));
-% 
-%         %Correct the position
-%         x = x * configs.bin_size/current_scale;
-%         y = y * configs.bin_size/current_scale;
-%         width = floor(char_dims(2)/current_scale);
-%         height = floor(char_dims(1)/current_scale);
-% 
-%         bbType = ones(length(x),1)*models{model_index}.char_index;
-%         bbs = [x,y,repmat(width,length(x),1),repmat(height,...
-%                length(x),1),scores,bbType];
-% 
-%         current_count = size(bbs,1);
-%         if current_count > 0
-%             total_bbs(total+1:total+current_count,:) = bbs;
-%             total = total + current_count;
-%         end
-%     end
-% end
-% 
-% bbs=total_bbs(1:total,:);
-% end
+function bbs=getbbs(I,models,hmsPath)
+% Return the bbs from the previously collected hms
+%
+% INPUTS:
+%   I - image
+%   models - two layers cell-array, models{charindex,mixtureindex}
+%       
+% OUTPUTS:
+%   hms - one layer cell-array, hms{charindex} the heatmap response is
+%     combined through taking the max of all the response.
+%   scales - the scales used for detections
+configs = configsgen;
+% load the hms
+lstruct = load(hmsPath);
+hms = lstruct.hms;
+scales = lstruct.scales;
+bbs = getbbsHelper(models,hms,scales,configs);
+end
+
+function bbs=getbbsHelper(models,hms,scales,configs)
+initthres = configs.initThres;
+total_bbs = zeros(1e6,6);
+total = 0;
+nScales = length(scales);
+for iLevel=1:nScales
+    hmsAtCurrentScale = hms{iLevel};
+    if isempty(hmsAtCurrentScale); continue; end;
+    current_scale = scales(iLevel);
+    for iModel = 1:size(models,1)
+        currentModel = models{iModel,1};
+        charDims = currentModel.char_dims;
+        hm = hmsAtCurrentScale{iModel} + currentModel.bias;
+        
+        ind = find(hm > initthres); % Get the locations of the response
+        [y,x] = ind2sub(size(hm),ind);
+        if (isempty(x)); continue; end;
+        
+        scores = hm(ind);
+        if (size(x,2) > 1); x = x'; y = y'; scores = scores'; end
+        %assert(length(x)==length(scores));
+
+        %Correct the position
+        x = x * configs.bin_size/current_scale;
+        y = y * configs.bin_size/current_scale;
+        width = floor(charDims(2)/current_scale);
+        height = floor(charDims(1)/current_scale);
+        
+        ind = strfind(configs.alphabets,currentModel.char_index);
+        bbType = ones(length(x),1)*ind;
+        bbs = [x,y,repmat(width,length(x),1),repmat(height,...
+               length(x),1),scores,bbType];
+
+        currentCount = size(bbs,1);
+        if currentCount > 0
+            total_bbs(total+1:total+currentCount,:) = bbs;
+            total = total + currentCount;
+        end
+    end
+end
+
+bbs=total_bbs(1:total,:);
+end
--- chardetexp.m
+++ chardetexp.m
@@ -1,6 +1,7 @@
 % Given that you have mixtures of models, this script is to run and output
 % the performance on the ICDAR dataset
 configs=configsgen;
+[dPath,ch,ch1,chC,chClfNm,dfNP]=globals;
 dataset_path = fullfile(configs.icdar,'test');
 imagesPath = fullfile(dataset_path,'images');
 modelPath = 'mixture_models';
@@ -11,15 +12,64 @@ models = loadMixtureModels(modelPath);
 %% get the image list
 imgLst = dir(fullfile(imagesPath,'*.jpg'));
 nImg = length(imgLst);
+hmsPath = fullfile('hms');
+mkdir(hmsPath);
 
 %% Get the heatmap
 savehms = @(sf,hms,scales) save(sf,'hms','scales');
-for i=1:nImg
+parfor i=1:nImg
+  i
+  savePath = fullfile(hmsPath,sprintf('%s.mat',imgLst(i).name))
+  if exist(savePath,'file'); continue; end;
+  imgPath = fullfile(imagesPath,imgLst(i).name);
+  I = imread(imgPath);
+  tic; [hms,scales] = charDetMixtures('gethm',I,models); toc;
+  savehms(savePath,hms,scales);
+end
+
+%% Get the bbs
+savebbs = @(sf,bbs) save(sf,'bbs');
+dtDirMix = fullfile(dPath,'icdar','test','det_results_mix');
+parfor i=1:nImg
   i
   imgPath = fullfile(imagesPath,imgLst(i).name);
   I = imread(imgPath);
-  [hms,scales] = charDetMixtures('gethm',I,models);
-  % save the hms and the scales
+  currentHmsPath = fullfile(hmsPath,sprintf('%s.mat',imgLst(i).name));
+  bbs = charDetMixtures('getbbs',I,models,currentHmsPath);
+  bbs = bbNms(bbs,'type','max','overlap',.5,'separate',1);
+  savePath = fullfile(dtDirMix,sprintf('%s.mat',imgLst(i).name));
+  savebbs(savePath,bbs);
 end
 
-%% size(models,1)Get the bbs
+%% Get the f-score
+gtDir = fullfile(dPath,'icdar','test','charAnn');
+dtDir = fullfile(dPath,'icdar','test','det_results');
+fscores = zeros(20,2);
+ticId = ticStatus('Collecting Fscore',1,30,1);
+for iChar = 1
+  iChar
+  currentChar = ch(iChar);
+  tocStatus(ticId,iChar/(length(ch)-1));
+  [gt0,~] = bbGt('loadAll',gtDir,[],{'lbls',currentChar});
+  gt0 = gt0(1:15);
+  dtmix = loadBB(dtDirMix,iChar);
+  dtsynth = loadBB(dtDir,iChar);
+  for i=1:length(dtsynth)
+    bbs = dtsynth{i};
+    dtsynth{i} = bbNms(bbs,'type','max','overlap',.5,'separate',1);
+  end
+  
+  % Computing score for mix
+  [gtm,dtm] = bbGt( 'evalRes', gt0, dtmix);
+  [xsm,ysm,~]=bbGt('compRoc', gtm, dtm, 0);
+  fs = Fscore(xsm,ysm);
+  fscores(iChar,1) = fs;
+  
+  % Computing score for synth
+  dtsynth = dtsynth(1:15);
+  [gts,dts] = bbGt( 'evalRes', gt0, dtsynth);
+  [xss,yss,~]=bbGt('compRoc', gts, dts, 0);
+  fs = Fscore(xss,yss);
+  fscores(iChar,2) = fs;
+  fscores
+end
